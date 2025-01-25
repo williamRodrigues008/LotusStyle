e9405d03cb4d59369a94b969cdacc293df558448
William Rodrigues da Silva <wrsilva008@gmail.com>
2025-01-25 12:10:40 -0300

    [dev] - "criação de contexto do banco"

---
+++ LotusStyle.API/Datas/DbContexto.cs
@@ -0,0 +1,14 @@
+﻿using LotusStyle.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LotusStyle.API.Datas
+{
+    public class DbContexto : DbContext
+    {
+        public DbContexto(DbContextOptions<DbContexto> options) : base(options) { }
+        
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<ProdutoVendido> ProdutoVendido { get; set; }
+
+    }
+}
