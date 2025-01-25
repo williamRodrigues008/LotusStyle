0990b9fb84745414fe6fc4b3bf75176dcb8d73c2
William Rodrigues da Silva <wrsilva008@gmail.com>
2025-01-25 09:50:11 -0300

    [dev] - "Criação de interfaces de produtos para leitura e escrita"

---
+++ LotusStyle.API/Interfaces/IEscritaProduto.cs
@@ -0,0 +1,11 @@
+﻿using LotusStyle.API.Entities;
+
+namespace LotusStyle.API.Interfaces
+{
+    public interface IEscritaProduto
+    {
+        Produto CriarProduto(Produto produto);
+        Produto AtualizarProduto(Produto produto);
+        Produto RemoverProduto(int id);
+    }
+}
---
+++ LotusStyle.API/Interfaces/ILeituraProduto.cs
@@ -0,0 +1,16 @@
+﻿using LotusStyle.API.Entities;
+
+namespace LotusStyle.API.Interfaces
+{
+    public interface ILeituraProduto
+    {
+        List<Produto> BuscarTodosProdutos();
+        List<Produto> BuscarProdutosPorMarca(string marca);
+        List<Produto> BuscarProdutosPorTipo(string tipo);
+        List<Produto> BuscarProdutosPorNome(string nome);
+        List<Produto> BuscarProdutosPedidosPorUltimo();
+        Produto BuscarProdutosPorId(int id);
+        List<Produto> BuscarProdutosMaisPedidos(int id);
+
+    }
+}
