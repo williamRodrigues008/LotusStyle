83f8373467cbf48f25cc77c423af55ec9ec2df14
William Rodrigues da Silva <wrsilva008@gmail.com>
2025-01-25 09:30:05 -0300

    [master] - "Criação de entity produto. obs.:  a partir desse commit, todos serão feitos para nova branch (dev) e irá para master através de pullrequest"

---
+++ LotusStyle.API/Entities/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace LotusStyle.API.Entities
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public int CodProduto { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public string? Marca { get; set; }
+        public string? Tipo { get; set; }
+        public string? LinhaProduto { get; set; }
+        public string? NacionalImportado { get; set; }
+        public string? Imagem { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
