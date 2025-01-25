64a89aad5977913014dcd4bc1419f3a8dc878d28
William Rodrigues da Silva <wrsilva008@gmail.com>
2025-01-25 12:11:40 -0300

    [dev] - "criação de classe de produtos vendidos para filtros de mais vendidos e ultimos vendidos"

---
+++ LotusStyle.API/Entities/ProdutoVendido.cs
@@ -0,0 +1,9 @@
+﻿namespace LotusStyle.API.Entities
+{
+    public class ProdutoVendido
+    {
+        public int Id { get; set; }
+        public Produto? Produto { get; set; }
+        public DateTime? DataVenda { get; set; }
+    }
+}
