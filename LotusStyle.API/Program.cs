409470d4f3ac549dc413589e1062273a201a4ccd
William Rodrigues da Silva <wrsilva008@gmail.com>
2025-01-27 17:06:30 -0300

    [dev] - "implementação de injeção de dependencia na program"

--- LotusStyle.API/Program.cs
+++ LotusStyle.API/Program.cs
@@ -1,3 +1,5 @@
+using LotusStyle.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Infraestrutura(builder.Configuration);
 
 var app = builder.Build();
 
@@ -17,9 +20,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
