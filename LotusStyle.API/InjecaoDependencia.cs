49daaa4294dcbdc68a20b5dc66f91ca7195e00e2
William Rodrigues da Silva <wrsilva008@gmail.com>
2025-01-27 17:04:22 -0300

    [dev] - "criação da classe de injeção de dependencias"

---
+++ LotusStyle.API/InjecaoDependencia.cs
@@ -0,0 +1,23 @@
+﻿using LotusStyle.API.Datas;
+using LotusStyle.API.Interfaces;
+using LotusStyle.API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace LotusStyle.API
+{
+    public static class InjecaoDependencia
+    {
+        public static IServiceCollection Infraestrutura(this IServiceCollection service, IConfiguration config)
+        {
+            service.AddDbContext<DbContexto>(op =>
+            {
+                op.UseSqlServer(config.GetConnectionString("ConnectionString"),
+                    m => m.MigrationsAssembly(typeof(DbContexto).Assembly.FullName));
+            });
+
+            service.AddScoped<ILeituraEscritaProduto, ProdutosService>();
+
+            return service;
+        }
+    }
+}
